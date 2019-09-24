e091a5a6b6af11b1c0af1289c9cc54168ddebb84
Kolya Opahle <k.opahle@sap.com>
2019-09-24 20:21:28 +0200

    Initial Commit

---
+++ MensaCardSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MensaCardSwiftUI
+//
+//  Created by Opahle, Kolya on 24.09.19.
+//  Copyright © 2019 Opahle, Kolya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
